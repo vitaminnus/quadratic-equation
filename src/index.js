c4d258d81ca5e805927ea2794851a99e982a2be3
Viktor Dmitriev <vitaminnus@gmail.com>
2018-02-24 14:54:50 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const regexp = /[^\^]-? ?[0-9]+/g;
+  let result = equation.match(regexp);  
+  result = result.map(element => element.replace(/ /g, ''));
+  const a = Number(result[0]),
+        b = Number(result[1]),
+        c = Number(result[2]);
+  const x1 = Math.round((-1*b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a));
+  const x2 = Math.round((-1*b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a));
+  return (x1 < x2) ? [x1, x2] : [x2, x1];
 }
